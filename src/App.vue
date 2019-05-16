958ecb6675774ffc19efb167fabe00076abf06d1
ppranitps20 <ppranitps20@gmail.com>
2019-05-16 08:43:38 +0530

    Changed project structure

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1 class="title is-centered">Vue Calendar</h1>
+    <CalendarWeek/>
+    <CalendarEntry/>
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
+
+export default {
+  name: 'app',
+  components: {
+    CalendarWeek,
+    CalendarEntry,
+  },
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  height:auto;
+  background: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+}
+
+.title {
+  padding-bottom: 10vh;
+  padding-top: 5vh;
+  font-size: 4em;
+  background: -webkit-linear-gradient(#2ecc71, #3498db);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-multiline">
+      <CalendarDay v-for="day in sharedState.seedData" :key="day.id" :day="day"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from '../store';
+import CalendarDay from './CalendarDay.vue';
+
+export default {
+  name: 'CalendarWeek',
+  data() {
+    return {
+      sharedState: store.state,
+    };
+  },
+  components: {
+    CalendarDay,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.columns {
+  display: flex;
+  flex-wrap: wrap;
+  flex-flow:row;
+  justify-content: flex-start;
+  margin: 0 auto;
+}
+
+#calendar-week {
+  margin: 0px 10px 50px 10px;
+  .column {
+    padding: 0 0 0 0;
+    margin: 10px;
+    flex-basis: 20em;
+    flex-grow: 0;
+  }
+}
+</style>
